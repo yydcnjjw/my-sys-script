af90c285f1c5177fa935bb82526ca53d6ebdc556
yydcnjjw <yydcnjjw@gmail.com>
2020-11-14 14:47:14 +0800

    openvpn boot

---
+++ openvpn_boot.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import subprocess
+import argparse
+import re
+
+# def str2bool(v):
+#     if isinstance(v, bool):
+#        return v
+#     if v.lower() in ('yes', 'true', 't', 'y', '1'):
+#         return True
+#     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+#         return False
+#     else:
+#         raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def main():
+    parser = argparse.ArgumentParser(description='Args')
+
+    parser.add_argument(
+        '-R',
+        '--remove-route',
+        action='store_const',
+        const=True,
+        default=False,
+        help='remove route'
+    )
+
+    args = parser.parse_args()
+
+    ps = subprocess.run(['ps', '-A'], capture_output=True)
+    if re.search('openvpn', str(ps.stdout)) is not None:
+        print('openvpn is running')
+        return
+
+    default_net_addr = '128.129.0'
+    is_default_net = False
+    while not is_default_net:
+        openvpn = subprocess.Popen(['openvpn', '/etc/openvpn/openvpn_client_internal.conf'], stdout=subprocess.PIPE)
+        route_list = []
+        try:
+            while openvpn.poll() == None:
+                line = str(openvpn.stdout.readline())
+
+                # match net addr
+                m = re.findall(r'net_addr_v4_add: ([^ ]+)', line)
+                if len(m) != 0:
+                    print('openvpn use ip: ', m[0])
+                    if default_net_addr not in m[0]:
+                        print('openvpn kill for use default ip: ', m[0])
+                        openvpn.kill()
+                        break
+                    else:
+                        is_default_net = True
+
+                # match net route
+                m = re.findall(r'net_route_v4_add: ([^ ]+)', line)
+                if len(m) != 0:
+                    print('openvpn add route: ', m[0])
+                    route_list.append(m[0])
+
+                # match end
+                m = re.search(r'Initialization Sequence Completed', line)
+                if m is not None:
+                    break
+
+            if is_default_net and args.remove_route:
+                for route in route_list:
+                    print('openvpn del route: ', route)
+                    subprocess.run(['ip', 'route', 'del', route, 'via', '128.129.0.1'])
+        except Exception:
+            openvpn.kill()
+
+    
+if __name__ == '__main__':
+    main()
